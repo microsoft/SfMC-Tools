c7fbcab50ef4cace179a02b0bb8adf645172d1ae
Kevin Bloom <96195095+KevinBloom-MSFT@users.noreply.github.com>
2022-06-07 16:11:54 -0500

    Update Post-SfMCDiscovery.ps1

--- Exchange/DiscoveryScripts/Post-SfMCDiscovery.ps1
+++ Exchange/DiscoveryScripts/Post-SfMCDiscovery.ps1
@@ -1,4 +1,27 @@
-﻿param(
+﻿<#//***********************************************************************
+//
+// Post-SfMCDiscovery.ps1
+// Modified 14 January 2022
+// Last Modifier:  Jim Martin
+// Project Owner:  Jim Martin
+// Version: v4.1
+//
+//***********************************************************************
+//
+// Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//**********************************************************************​
+#>
+ 
+ param(
     [Parameter(Mandatory=$false)] [string]$OutputPath
 )
 function Get-FolderPath {   
